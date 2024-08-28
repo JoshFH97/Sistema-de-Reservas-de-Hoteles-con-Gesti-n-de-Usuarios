b5980f96fd7e7ca981595221bfae59fdd58c2cc0
JoshFH97 <joshelxd@gmail.com>
2024-08-28 16:03:46 -0600

    xxxxxxx

--- consultas/indice.sql
+++ consultas/indice.sql
@@ -1,3 +1,3 @@
 CREATE INDEX idx_reservas
 ON Reservas (habitacionId);
--- La Razon es que es la tabla a la que mas consultas se hace y si casi todas relacionadas con la columna habitacionId
+-- La Razon es que es la tabla a la que mas consultas se hace y si casi todas relacionadas con la columna habitacionId\
