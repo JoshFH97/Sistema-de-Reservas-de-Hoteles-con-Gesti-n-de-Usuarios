809d67fef0429b7c6412505784cd1d622727bce1
JoshFH97 <joshelxd@gmail.com>
2024-08-28 09:22:04 -0600

    avance de consultas

--- consultas/pruebas.sql
+++ consultas/pruebas.sql
@@ -9,15 +9,30 @@ JOIN Hoteles ON Habitaciones.hotel_id = Hoteles.id
 GROUP BY Hoteles.id
 ORDER BY total_reservas DESC;
 
--- Consulta para contar cuántas habitaciones disponibles hay en un hotel específico en una fecha dada.'
-SELECT Hoteles.nombre, Reservas.HabitacionId, Habitaciones.estado
-FROM Reservas
-JOIN Habitaciones ON Reservas.HabitacionId = Habitaciones.id
-JOIN Hoteles ON Habitaciones.estado = Hoteles.id;
+-- -- Consulta para contar cuántas habitaciones disponibles hay en un hotel específico en una fecha dada.'
+-- SELECT COUNT(Habitaciones.id) AS HabitacionesDisponibles
+-- FROM Habitaciones
+-- LEFT JOIN Reservas 
+-- ON Habitaciones.id = Reservas.HabitacionId 
+-- AND '2024-08-27' BETWEEN Reservas.Fecha_de_Inicio AND Reservas.Fecha_de_Fin
+-- JOIN Hoteles 
+-- ON Habitaciones.hotel_id = Hoteles.id
+-- WHERE Hoteles.nombre = 'Nombre del Hotel'
+-- AND (Reservas.HabitacionId IS NULL OR Habitaciones.estado = 'Disponible');
 
 
 
 -- Consulta para buscar hoteles por nombre.
+  SELECT nombre, ubicacion, descripcion
+  FROM Hoteles
+  WHERE nombre = 'Hotel Reforma';
 
 -- Consulta para buscar hoteles cuya ubicación comienza con un texto específico.
+   SELECT nombre, ubicacion, descripcion
+   FROM Hoteles
+   WHERE ubicacion LIKE 'gua%'
+
 -- Consulta para buscar hoteles cuya ubicación termina con un texto específico.
+   SELECT nombre, ubicacion, descripcion
+   FROM Hoteles
+   WHERE ubicacion LIKE '%jara'
