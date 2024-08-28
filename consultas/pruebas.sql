22e455f23acd2bef5e8ba3ff7081a21df8995812
IsaacFerreto <168207077+IsaacFerreto@users.noreply.github.com>
2024-08-28 08:27:50 -0600

    nuevas consultas

---
+++ consultas/pruebas.sql
@@ -0,0 +1,23 @@
+use Booking;
+
+/********************************************************************************************************/
+--Consulta para obtener los hoteles con mayor número de reservas.
+SELECT Hoteles.nombre, COUNT(Reservas.id) AS total_reservas
+FROM Reservas
+JOIN Habitaciones ON Reservas.HabitacionId = Habitaciones.id
+JOIN Hoteles ON Habitaciones.hotel_id = Hoteles.id
+GROUP BY Hoteles.id
+ORDER BY total_reservas DESC;
+
+-- Consulta para contar cuántas habitaciones disponibles hay en un hotel específico en una fecha dada.'
+SELECT Hoteles.nombre, Reservas.HabitacionId, Habitaciones.estado
+FROM Reservas
+JOIN Habitaciones ON Reservas.HabitacionId = Habitaciones.id
+JOIN Hoteles ON Habitaciones.estado = Hoteles.id;
+
+
+
+-- Consulta para buscar hoteles por nombre.
+
+-- Consulta para buscar hoteles cuya ubicación comienza con un texto específico.
+-- Consulta para buscar hoteles cuya ubicación termina con un texto específico.
--- consultas/pruebas_isaac.sql
+++ consultas/pruebas_isaac.sql
@@ -1,3 +1,7 @@
+
+-- Consulta para obtener las reservas de un cliente (por email) realizadas en el mes anterior.
+select * from reservas JOIN usuarios ON UsuarioId = usuarios.id WHERE `Fecha_de_Fin` < '2024-08-01' and email = 'david080900@gmail.com'
+
 -- Consulta para calcular el promedio de reservas diarias en un hotel.
 SELECT 
     COUNT(*) / COUNT(DISTINCT Fecha_de_Inicio) 
